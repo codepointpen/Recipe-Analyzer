b964bc32813436b9f6bf658267d83fab0713b471
haohelenhu <helenhaohu@gmail.com>
2026-01-08 21:51:58 -0800

    Added implementation of data_utils script and 01_exploration notebook

---
+++ src/data_utils.py
@@ -0,0 +1,19 @@
+# src/recipe_analysis.py
+
+import pandas as pd
+import numpy as np
+
+class RecipeAnalysis:
+    def __init__(self, file_path):
+        """
+        initialize with dataset path
+        """
+        
+
+    def preprocess(self):
+        """
+        identify numeric, binary, and continuous columns
+        """
+        
+
+    
